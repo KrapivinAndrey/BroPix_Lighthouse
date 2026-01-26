924b1ea600863372e1f762f312899bf221377758
krapivin_andrey <krapivin_andrey@mail.ru>
2026-01-26 22:34:18 +0500

    init

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""
+LighthouseForCycles - Main package
+"""
+
+__version__ = "0.1.0"
---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+"""
+Tests for main module
+"""
+
+import unittest
+from src.main import main
+
+
+class TestMain(unittest.TestCase):
+    """Test cases for main function"""
+
+    def test_main(self):
+        """Test main function"""
+        # This is a placeholder test
+        self.assertIsNone(main())
+
+
+if __name__ == "__main__":
+    unittest.main()
